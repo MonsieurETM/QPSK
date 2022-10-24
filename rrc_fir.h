19046dd79146b7926a1db2fb187d7f16937d50dd
MonsieurETM <115579347+MonsieurETM@users.noreply.github.com>
2022-10-24 16:41:28 -0500

    Create rrc_fir.h

---
+++ rrc_fir.h
@@ -0,0 +1,27 @@
+/*
+ * rrc_fir.h
+ */
+
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include <complex.h>
+
+#ifndef M_PI
+#define M_PI          3.14159265358979323846
+#endif
+
+#define TAU           (2.0 * M_PI)
+
+#define NTAPS         127	// lower bauds need more taps, 127 for 300 baud is good
+#define GAIN          1.5
+
+void rrc_fir(complex float [], complex float [], int);
+void rrc_make(float, float, float);
+
+#ifdef __cplusplus
+}
+#endif
