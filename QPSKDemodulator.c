b4c3f28b45718c47ab39101f5a8338c2cd5aa6a2
MonsieurETM <115579347+MonsieurETM@users.noreply.github.com>
2023-01-08 07:10:09 -0600

    Update QPSKDemodulator.c

--- QPSKDemodulator.c
+++ QPSKDemodulator.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Dennis Sheirer
+ * Copyright (C) 2002,2012 Free Software Foundation, Inc.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
